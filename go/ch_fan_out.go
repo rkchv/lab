c2d2143a47b07866f0b222b0645b95813e5cda0e
Roman Kochnev <rkochnevv@gmail.com>
2024-10-02 09:01:54 +0300

    fix: add sync

--- go/ch_fan_out.go
+++ go/ch_fan_out.go
@@ -3,25 +3,32 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
 func main() {
 	numWorkers := 100
 	ch := make(chan string, numWorkers)
+	wg := sync.WaitGroup{}
 
 	for w := 0; w < numWorkers; w++ {
+		wg.Add(1)
 		go func(w int) {
+			defer wg.Done()
 			duration := time.Duration(rand.Intn(100)) * time.Millisecond
 			time.Sleep(duration)
 			ch <- "take this cake"
 		}(w)
 	}
 
-	for numWorkers > 0 {
-		p := <-ch
-		numWorkers -= 1
-		fmt.Println(p)
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for v := range ch {
+		fmt.Println(v)
 	}
 
 }
